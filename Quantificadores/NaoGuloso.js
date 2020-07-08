92dcfbe0b167f0b650e91ed0eb388c9add76c7f2
Jonathan <jonathan.gsanto@hotmail.com>
2020-07-07 21:25:20 -0300

    nova

--- Quantificadores/NaoGuloso.js
+++ Quantificadores/NaoGuloso.js
@@ -1,1 +1,22 @@
 const texto = '<div>Conteudo 01</div><div>Conteudo 02</div>'
+
+//Quantificadores são gulosos por padrão (greedy)
+
+console.log(texto.match(/<div>.+<\/div>/g))
+console.log(texto.match(/<div>.*<\/div>/g))
+console.log(texto.match(/<div>.{0,100}<\/div>/g))
+
+///Quantificadores NÃO gulosos
+
+const texto = '<div>Conteudo 01</div><div>Conteudo 02</div>'
+
+console.log(texto.match(/<div>.+?<\/div>/g))
+console.log(texto.match(/<div>.*?<\/div>/g))
+console.log(texto.match(/<div>.{0,100}?<\/div>/g))
+
+
+
+ 
+
+
+
